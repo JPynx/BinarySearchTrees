760e8ddfda15eebff0a13f8863e525301c8c62c1
JPynx <jsanchez-espino24@kentdenver.org>
2024-03-05 10:18:10 -0700

    This is the first commit that has about half of BSTs

---
+++ src/BinarySearchTree.java
@@ -0,0 +1,129 @@
+/**
+ * JPSE
+ * Date: 2/26/2024
+ * Collaborators: N/A
+ **/
+public class BinarySearchTree<Key extends Comparable<Key>, Value> {
+
+    public Node<Key, Value> root;
+
+    public BinarySearchTree() {
+        root=null;
+    }
+
+    public int size() {
+
+        return size(root);
+    }
+
+    //use Node's recursive size
+    private int size(Node x) {
+        if (x == null) {
+            return 0;
+        } else {
+            return x.getSize();
+        }
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    //recursive put wrapper
+    public void put(Key key, Value value) {
+        root = put(root, key, value);
+    }
+
+    //recursive put
+    //sets left/right or creates a new node appropriately, returns the
+    //modified node n
+    private Node<Key, Value> put(Node<Key, Value> n, Key key, Value val) {
+        if (root == null) {
+            return new Node<Key, Value>(key, val, 1);
+        }
+    }
+
+    //recursive get wrapper
+    public Value get(Key key) {
+
+        return get(root, key);
+    }
+
+    //recursive get
+    //returns null if the key does not exist
+    private Value get(Node<Key, Value> n, Key key) {
+        if (n == null) {
+            return null;
+        }
+        int j = key.compareTo(n.getKey());
+        if (j < 0) {
+            return get(n.getLeft(), key);
+        } else if (i > 0) {
+            return get(n.getRight(), key);
+        } else {
+            return n.getValue();
+        }
+    }
+
+    public boolean contains(Key key) {
+        return get(key) != null;
+
+    }
+
+    public Value remove(Key key) {
+        Value v = get(key);
+        root = remove(root, key);
+        return v;
+    }
+
+    private Node remove(Node<Key, Value> n, Key key) {
+        if (n == null) return null;
+        int i = key.compareTo(n.getKey());
+        if (i < 0) {
+            n.setLeft(remove(n.getLeft(), key));
+        } else if (i > 0) {
+            n.setRight(remove(n.getRight(), key));
+        } else {
+            if (n.getRight() == null) return n.getLeft();
+            if (n.getLeft() == null) return n.getRight();
+            Node min = min(n.getRight());
+            min.setLeft(n.getLeft());
+            n = n.getRight();
+        }
+        n.setSize(size(n.getRight()) + size(n.getLeft()) + 1);
+        return n;
+    }
+
+
+    //returns the node at the left most left branch of n
+    private Node<Key, Value> min(Node<Key, Value> n) {
+
+    }
+
+    public Key max() {
+        return max(root).getKey();
+    }
+
+    public Key min() {
+        return min(root).getKey();
+    }
+
+    //returns the node at the right most right branch of n
+    private Node<Key, Value> max(Node<Key, Value> n) {
+
+    }
+
+    public String toString() {
+        String temp = toString(root);
+        temp = temp.substring(0, temp.length() - 2);
+        return "{" + temp + "}";
+    }
+
+    private String toString(Node<Key, Value> n) {
+        if (n == null) return "";
+        return toString(n.getLeft()) +
+                n.getKey() + "=" + n.getValue() + ", " +
+                toString(n.getRight());
+
+    }
+}
